55541523e2ab1f1e0b382a69c3dcce8ba8158132
Sam Dawson <sam.dawson@affinitylive.com>
2016-07-31 20:53:32 +1000

    Add count for hex codes

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ var program = require('commander');
 var fs = require('fs');
 var sc_color = require("sc-color");
 var Handlebars = require('handlebars');
+var glob = require('glob');
 var path = require('path');
 var appPath = path.dirname(require.main.filename);
 
-program.arguments('<file>')
-	.action(function(file) {
-		fs.readFile(file, 'utf8', processFile);
+program.arguments('<file> [sassPath]')
+	.action(function(file, sassPath) {
+		var contents = fs.readFileSync(file, 'utf8', processFile);
+		processFile(contents, sassPath);
 	})
 	.parse(process.argv);
 
-function processFile(err, contents) {
+function processFile(contents, sassPath) {
 	var colors = findColors(contents);
 	var sortedColors = sortColors(colors);
+	appendHexCountData(sortedColors, sassPath);
 	var data = { 
 		colors: sortedColors,
 		appPath: appPath
@@ -36,11 +39,37 @@ function findColors(contents) {
 	return found;
 }
 
-function sortColors(colors){
+function sortColors(colors) {
 	var sorted = colors.sort(function(colorA, colorB) {
 	    return sc_color('#' + colorA.hex).hue() - sc_color('#' + colorB.hex).hue();
 	});
 	return sorted;
+}
+
+function appendHexCountData(sortedColors, sassPath) {
+	var allHexCodes = findAllHexCode(sassPath);
+	sortedColors.forEach(function (sortedColor) {
+		sortedColor.count = allHexCodes[sortedColor.hex].count;
+	});
+}
+
+function findAllHexCode(sassPath) {
+	var scssFiles = glob.sync(sassPath + "/**/*.scss", {});
+	var regex = /(?:#)(.*)(?:\;)/g;
+	var found = {};
+	scssFiles.forEach(function (scssFile) {
+		var scssContents = fs.readFileSync(scssFile, 'utf8');
+		var matches;
+		while( matches = regex.exec(scssContents) ) {
+			var colorobject = found[matches[1]];
+			if (colorobject) {
+				colorobject.count++;
+			} else {
+				found[matches[1]] = {count: 1};
+			}
+		}
+	});
+	return found;
 }
 
 function generateHTML(data) {
